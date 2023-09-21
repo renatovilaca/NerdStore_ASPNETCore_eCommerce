da8baffbe75e0f2be9716e166e5b84337e3b73a1
Renato Vilaça <renatovilaca@gmail.com>
2023-09-20 22:15:16 -0300

    Implemented Register/Login for Identity API

---
+++ src/services/NSE.Identity.API/Models/UserViewModels.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NSE.Identity.API.Models
+{
+    public class UserViewModels
+    {
+        public class UserRegister
+        {
+            [Required(ErrorMessage = "The field {0} is required")]
+            [EmailAddress(ErrorMessage = "The field {0} está em formato inválido")]
+            public string Email { get; set; }
+
+            [Required(ErrorMessage = "The field {0} is required")]
+            [StringLength(100, ErrorMessage = "The field {0} must be between {2} and {1} characters", MinimumLength = 6)]
+            public string Password { get; set; }
+
+            [Compare("Password", ErrorMessage = "The passwords does not match")]
+            public string ConfirmPassword { get; set; }
+        }
+
+        public class UserLogin
+        {
+            [Required(ErrorMessage = "The field {0} is required")]
+            [EmailAddress(ErrorMessage = "The field {0} has invalid format")]
+            public string Email { get; set; }
+
+            [Required(ErrorMessage = "The field {0} is required")]
+            [StringLength(100, ErrorMessage = "The field {0} must be between {2} and {1} characters", MinimumLength = 6)]
+            public string Password { get; set; }
+        }
+    }
+}
--- src/services/NSE.Identity.API/Program.cs
+++ src/services/NSE.Identity.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using NSE.Identity.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,14 +18,26 @@ builder.Services.AddDefaultIdentity<IdentityUser>()
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "NerdStore Enterprise Identity API",
+        Description = "",
+        Contact = new OpenApiContact() { Name = "Renato", Url = new Uri("https://github.com/renatovilaca") },
+        License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/license/mit") }
+    });
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    });
 }
 
 app.UseHttpsRedirection();
